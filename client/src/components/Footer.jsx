cc2e2f635da2cdddd5467df489d149a59ee819d0
Vedp6 <vedantpatil15208@gmail.com>
2026-09-03 15:43:25 +0530

    Completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -54,9 +54,9 @@ const Footer = () => {
         </div>
       </div>
       <p className="pt-4 text-center text-xs md:text-sm pb-5">
-        Copyright 2025 ©{" "}
-        <a target="_blank" href="https://elyse502.github.io/Elysee-Portfolio">
-          ElyséeDev
+        Copyright 2026 ©{" "}
+        <a target="_blank" href="https://personal-portfolio-five-chi-32.vercel.app">
+          Vedant Patil
         </a>
         . All Right Reserved.
       </p>
